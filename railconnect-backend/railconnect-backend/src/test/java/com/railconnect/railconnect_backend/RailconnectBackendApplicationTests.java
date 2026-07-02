42df48c826b3e8253083ba21d4e1c0da3772a2ee
Himanshu Jagannath Chavan <chavanhimanshu37@gmail.com>
2026-07-02 15:00:02 +0530

    spring boot dependencies

---
+++ railconnect-backend/railconnect-backend/src/test/java/com/railconnect/railconnect_backend/RailconnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railconnect.railconnect_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailconnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
